a66f9582261d79453239269c6e438be6a06adfdf
payank <94164757+payank@users.noreply.github.com>
2022-03-29 15:57:14 +0530

    Update MuiRating.tsx
    Add comments

--- react-mui-demo/src/components/MuiRating.tsx
+++ react-mui-demo/src/components/MuiRating.tsx
@@ -3,9 +3,13 @@ import { useState } from 'react'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 
+
+//for rating in stars (*), like glassdoor/google etc
 export const MuiRating = () => {
   const [value, setValue] = useState<number | null>(3)
   console.log(value)
+  
+  //accept two parms, 1 is the event and another one is the new value to be set
   const handleChange = (
     _event: React.ChangeEvent<{}>,
     newValue: number | null
@@ -14,6 +18,8 @@ export const MuiRating = () => {
   }
   return (
     <Stack spacing={2}>
+      //precision props allows us to select rating in decimal points
+      //icon props allows to use custom icons instead of start
       <Rating
         value={value}
         onChange={handleChange}
@@ -22,6 +28,7 @@ export const MuiRating = () => {
         icon={<FavoriteIcon fontSize='inherit' color='error' />}
         emptyIcon={<FavoriteBorderIcon fontSize='inherit' />}
         readOnly
+        highlisghSelectedOnly //highlight only tha maximum selected rating, used for happy/sad for max/min rating
       />
     </Stack>
   )
