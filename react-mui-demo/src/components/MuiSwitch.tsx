c9aaadacddba160053165e48e0544015331db75c
payank <94164757+payank@users.noreply.github.com>
2022-03-29 15:48:35 +0530

    Update MuiSwitch.tsx
    Add comments

--- react-mui-demo/src/components/MuiSwitch.tsx
+++ react-mui-demo/src/components/MuiSwitch.tsx
@@ -2,6 +2,8 @@ import { Box, FormControlLabel, Switch } from '@mui/material'
 import { useState } from 'react'
 
 export const MuiSwitch = () => {
+  
+  //for group of switch see the example in MuiCheckbox.tsx, only thing is to replace the checkbox with switch
   const [checked, setChecked] = useState(false)
   console.log(checked)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
