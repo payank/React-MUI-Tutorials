564a48e0349cbbff473c48935595b518803893a0
payank <94164757+payank@users.noreply.github.com>
2022-03-29 14:29:38 +0530

    Update MuiTypography.tsx

--- react-mui-demo/src/components/MuiTypography.tsx
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -6,9 +6,11 @@ export const MuiTypography = () => {
       <Typography variant='h1'>h1 Heading</Typography>
       <Typography variant='h2'>h2 Heading</Typography>
       <Typography variant='h3'>h3 Heading</Typography>
+      //h4 tag will become as h1 beacouse of component
       <Typography variant='h4' component='h1' gutterBottom>
         h4 Heading
-      </Typography>
+      </Typography> 
+      
       <Typography variant='h5'>h5 Heading</Typography>
       <Typography variant='h6'>h6 Heading</Typography>
       <Typography variant='subtitle1'>subtitle1</Typography>
