ce51f27f44e8603f8cc327105fa9c73fb539693e
payank <94164757+payank@users.noreply.github.com>
2022-03-29 15:08:38 +0530

    Update MuiTextField.tsx

--- react-mui-demo/src/components/MuiTextField.tsx
+++ react-mui-demo/src/components/MuiTextField.tsx
@@ -10,15 +10,20 @@ export const MuiTextField = () => {
   }
   return (
     <Stack spacing={4}>
+      
       <Stack spacing={2} direction='row'>
         <TextField label='Outlined' variant='outlined' />
         <TextField label='Filled' variant='filled' />
         <TextField label='Standard' variant='standard' />
       </Stack>
+      
       <Stack spacing={2} direction='row'>
         <TextField label='Small secondary' size='small' color='secondary' />
       </Stack>
+      
       <Stack spacing={2} direction='row'>
+        //required will place astrisk on the label
+        //handelling error state as well with the help of required, error and helperText props
         <TextField
           label='Form Input'
           required
@@ -26,16 +31,21 @@ export const MuiTextField = () => {
             !value ? 'Required' : 'Do not share your password with anyone'
           }
           type='password'
+          //disabled
           error={!value}
           value={value}
           onChange={handleChange}
         />
       </Stack>
+      
+      
       <Stack spacing={2} direction='row'>
+        //InputAdorment hels to suffix/prefix text, icons etc  (like visibilty icon for password)
         <TextField
           label='Amount'
           InputProps={{
             startAdornment: <InputAdornment position='start'>$</InputAdornment>
+            //readOnly:true
           }}
         />
         <TextField
